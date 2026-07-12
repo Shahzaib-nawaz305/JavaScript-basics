1e32700ffbf7d850936a0a2a881be899b5564b83
Shahzaib-nawaz305 <shahzaib0awan1@gmail.com>
2026-07-12 15:05:09 +0500

    Add JavaScript fundamentals practice examples

---
+++ day1.js
@@ -0,0 +1,45 @@
+// JavaScript Fundamentals Practice Day 1
+// let Variable
+let name="Ahmad";
+Console.log(name);
+
+//Const Variable 
+Const Country = "Pakistan";
+Console.log (Country);
+
+//Number Data Type
+let age = 25;
+Console.log(age);
+
+//String Data type
+let name = "Ahmad";
+Console.log(name);
+
+//Undefined Data type
+let City;
+Console.log(City);
+
+//Null Data Type
+let phone = "Null"
+Console.log(phone);
+
+//Object Data Type
+let student = {
+  name:"Ahmad",
+  age: 24
+};
+Console.log(student);
+
+//Array Data Type
+let Colors = ["Red", "Blue", "Green"];
+console.log(Colors);
+
+//Boolean Data Type
+let isStudent = true;
+console.log(isStudent);
+
+//Arithmetic Operator
+let a = 10;
+let b = 5;
+Console.log(a+b);
+
