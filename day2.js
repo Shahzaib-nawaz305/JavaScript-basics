c933144fcc627654b63de5b2b6abd201b1923f4d
Shahzaib-nawaz305 <shahzaib0awan1@gmail.com>
2026-07-13 09:34:20 +0500

    Add comparison operators examples in day2.js
    Added examples of comparison operators and their outputs.

--- day2.js
+++ day2.js
@@ -1,1 +1,25 @@
+//JavaScript Fundamentals Day 2 
+//Topic: Conditions and Logical Operators
+
+//Comparision Operators
+let a = 10;
+let b = 10;
+Console.log(a==b);
+//Output: true - both values are equal
+Console.log(a===b);
+//Output: true - value and data type both are same
+
+let x = "25";
+let y = 25;
+Console.log(x==y);
+//Output: true - Only values are compared
+Console.log(x===y);
+//Output: false - data types are different
+
+//Greater than and less than
+let num1 = 50;
+let num2 = 30;
+Console.log(num1>num2);
+//Output: true - 50 is greater than 30
+Console.log(num1<num2);
 
